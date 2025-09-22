2da5aa104dc8b64240c9c64819ebceacffc49806
lozukka <lotsulotsu@gmail.com>
2025-09-22 13:13:07 +0300

    Refactor cart logic and improve product handling
    Updated the add-to-cart functionality to use product IDs and improved product rendering by assigning unique IDs to each product. Added a new renderInCart function to display products in the cart, and refactored event handling for better maintainability. These changes enhance the cart's accuracy and scalability.

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ const productCards = document.getElementById("product-cards");
 const selectedCart = document.getElementById("selected-cart");
 const emptyCart = document.getElementById("empty-card");
 const totalItemsText =document.getElementById("total-items");
+const productsOnCart = document.getElementById("products-on-cart");
 const cartItems = [];
+let products = [];
 let totalItems = 0;
 let cartSum = 0;
 
@@ -14,17 +16,21 @@ document.querySelectorAll(".remove-item").forEach(btn =>{
 })
 
 document.addEventListener("click", (e) => {
-  if (e.target.closest(".add-to-cart")) {
-    console.log("added");
-    addToCart();
+  const btn = e.target.closest(".add-to-cart");
+  if (btn) {
+    const productCard = btn.closest(".product-card");
+    const id = productCard.dataset.id;
+    addToCart(id);
   }
 });
 
 async function getProducts() {
-  const products = await fetchProducts();
+  products = await fetchProducts();
 
-  products.forEach(({ image, name, category, price }) => {
-    renderProducts(image, name, category, price);
+  products.forEach((product) => {
+    product.id = generateId();
+    const {id, image, name, category, price} = product;
+    renderProducts(id, image, name, category, price);
   });
 }
 async function fetchProducts() {
@@ -39,8 +45,7 @@ async function fetchProducts() {
   }
 }
 
-function renderProducts(image, name, category, price) {
-  let id = generateId();
+function renderProducts(id, image, name, category, price) {
   let productCard = document.createElement("section");
   productCard.classList.add("product-card");
   productCard.setAttribute("data-id", `${id}`)
@@ -75,16 +80,63 @@ function generateId(){
   return Date.now().toString(36) + Math.random().toString(36).substr(2, 5);
 }
 
-function addToCart() {
-  
-  totalItems ++;
-  totalItemsText.textContent = totalItems;
+function addToCart(id) {
   if(selectedCart.classList.contains("hidden")){
     emptyCart.classList.replace("show", "hidden");
     selectedCart.classList.replace("hidden", "show");
   }
+const product = products.find(p => p.id == id);
+if(!product) {
+  console.warn ("Product can't be found with id: ", id);
+  return;
+}
+  renderInCart(product);
 
+  totalItems ++;
+  totalItemsText.textContent = totalItems;
+}
 
+function renderInCart(product){
+  let listProduct = document.createElement("span");
+  listProduct.classList.add("list-product");
+
+  let listProductText = document.createElement("div");
+  listProductText.classList.add("list-product-text");
+
+  let heading = document.createElement("h3");
+  heading.textContent = product.name;
+  listProductText.append(heading);
+  listProduct.append(listProductText);
+
+  let productPrices = document.createElement("span");
+  productPrices.classList.add("product-prices");
+
+  let productAmount = document.createElement("p");
+  productAmount.classList.add("product-amount");
+  productAmount.textContent = "1x";
+  productPrices.append(productAmount);
+
+  let onePrice = document.createElement("p");
+  onePrice.classList.add("one-price");
+  onePrice.textContent = `@$${product.price.toFixed(2)}`;
+  productPrices.append(onePrice);
+
+  let totalPrice = document.createElement("p");
+  totalPrice.classList.add("total-price");
+  totalPrice.textContent = `$${product.price.toFixed(2)}`;
+  productPrices.append(totalPrice);
+  listProductText.append(productPrices);
+  
+
+  let removeDiv = document.createElement("div");
+  removeDiv.classList.add("remove-item");
+  let removeIcon = document.createElement("img");
+  removeIcon.src = "./assets/images/icon-remove-item.svg";
+  removeIcon.atl = "Remove item from cart";
+  removeDiv.append(removeIcon);
+  listProduct.append(removeDiv);
+
+  productsOnCart.append(listProduct);
 }
 
 window.addEventListener("load", getProducts);
